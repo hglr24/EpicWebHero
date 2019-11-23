9e531f6d0eabff4cf750281becb260df258ee11f
clh910936 <carrielhunner@gmail.com>
2019-11-23 18:38:36 -0500

    created a rough draft ran num generator

--- random_num_gen.v
+++ random_num_gen.v
@@ -1,17 +1,21 @@
-module random_num_gen(clock, min, max, score, timeElapsed, ranNum);
-input clock;
-input [31:0] min, max;	//defining the range of the desired value
-input [31:0] score, timeElapsed;	//player's score, time since started
-output [31:0] ranNum;	//random value produced within the designated range
+module ran_num_generator(score, clk, ranNum, ranNumTen, ranNumLength);
+input clk;
+input [31:0] score;//, currentTime;
+output [31:0] ranNum, ranNumTen, ranNumLength;
 
-reg [31:0] inter, ranNum;
+reg [31:0] ranNum, ranNumTen;
+reg [31:0] counter;
 
-	initial begin
-		inter <= 0;
-	end
-	
-	always @(posedge clock) begin
-		inter <= timeElapsed / score;
-		ranNum <= ((max - min) * (inter - min) / (max - min)) + inter;
-	end
+initial begin
+		ranNum <= 32'd2;
+		counter <= 32'd0;
+end
+
+always @(posedge clk) begin
+	counter <= counter + 1;
+	ranNum = (3*ranNum + counter) % (score);	//generating a random value
+	ranNumTen = ranNum % 10;		//mapping that random value to between 0 and 9 for target selection
+	ranNumLength = (score % 5)
+end
+
 endmodule
