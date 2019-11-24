8d3a28ef821db86486f672b991e2953a72e72f0b
clh910936 <carrielhunner@gmail.com>
2019-11-23 19:39:31 -0500

    random num generator works for a value between 0 and 9

--- random_num_gen.v
+++ random_num_gen.v
@@ -1,4 +1,4 @@
-module ran_num_generator(score, clk, ranNum, ranNumTen, ranNumLength);
+module random_num_gen(score, clk, ranNum, ranNumTen, ranNumLength);
 input clk;
 input [31:0] score;//, currentTime;
 output [31:0] ranNum, ranNumTen, ranNumLength;
@@ -15,7 +15,7 @@ always @(posedge clk) begin
 	counter <= counter + 1;
 	ranNum = (3*ranNum + counter) % (score);	//generating a random value
 	ranNumTen = ranNum % 10;		//mapping that random value to between 0 and 9 for target selection
-	ranNumLength = (score % 5)
+
 end
 
 endmodule
