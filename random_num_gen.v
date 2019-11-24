33dcbed7bb89c84a6e994ba6291a8fcd354e7988
unknown <hglr24@gmail.com>
2019-11-23 21:10:38 -0500

    'tis functioning

--- random_num_gen.v
+++ random_num_gen.v
@@ -1,21 +1,29 @@
-module random_num_gen(score, clk, ranNum, ranNumTen, ranNumLength);
-input clk;
+module random_num_gen(score, clock, ranNum, ranNumTen, ranNumLength);
+input clock;
 input [31:0] score;//, currentTime;
-output [31:0] ranNum, ranNumTen, ranNumLength;
+output [31:0] ranNum, ranNumLength;
+output [3:0] ranNumTen;
 
-reg [31:0] ranNum, ranNumTen;
+reg [31:0] ranNum;
+reg [3:0] ranNumTen;
 reg [31:0] counter;
+reg [31:0] rand_count;
 
 initial begin
-		ranNum <= 32'd2;
+		ranNum <= 4'd2;
 		counter <= 32'd0;
+		rand_count <= 32'd0;
 end
 
-always @(posedge clk) begin
-	counter <= counter + 1;
-	ranNum = (3*ranNum + counter) % (score);	//generating a random value
-	ranNumTen = ranNum % 10;		//mapping that random value to between 0 and 9 for target selection
-
+always @(posedge clock) begin
+	if (counter >= 25000000) begin
+		ranNum = (3*ranNum + rand_count) % (score);	//generating a random value
+		ranNumTen = ranNum % 10;		//mapping that random value to between 0 and 9 for target selection
+		rand_count <= rand_count + 1;
+		counter <= 32'd0;
+	end else begin
+		counter <= counter + 1;
+	end
 end
 
 endmodule
