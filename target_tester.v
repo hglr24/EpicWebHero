55fa0177bf7424db6e181bdf4c02bacf090fa88e
unknown <hglr24@gmail.com>
2019-12-03 04:56:52 -0500

    PROOF OF COLNCEPT

--- target_tester.v
+++ target_tester.v
@@ -1,36 +1,34 @@
-module target_tester(clock, target_a, target_b);
-input clock;
+module target_tester(clock, target_a, target_b, photo_array, flex_r, flex_l, laser_r, laser_l, score_digit_a, score_digit_b, score_digit_c, score_digit_d);
 output [3:0] target_a, target_b;
+input clock, flex_l, flex_r;
+input [9:0] photo_array;
+output laser_r, laser_l;
+output [6:0] score_digit_a, score_digit_b, score_digit_c, score_digit_d;
 
 reg [3:0] target_a, target_b;
-reg [31:0] count;
 reg a;
+reg [31:0] score;
+
+laser_driver right_glove_laser(.clock(clock), .in(flex_r), .out(laser_r));
+laser_driver left_glove_laser(.clock(clock), .in(flex_l), .out(laser_l));
+
+score_converter s_c(score, score_digit_a, score_digit_b, score_digit_c, score_digit_d); // D is LSD, A is MSD
+
 initial begin
 		a <= 1;
-		count <= 0;
-		target_a <= 0;
-		target_b <= 0;
+		target_a <= 8;
+		target_b <= 9;
+		score <= 0;
+		
 end
 
-always @(posedge clock) begin
-	count <= count + 1;
-	if(count >= 32'd100000000) begin
-		count <= 32'd0;
-		if(a == 1) begin
-			target_a = target_a + 1;
-			if(target_a == 10) begin
-				target_a <= 0;
-				a <= 0;
-			end
-		end
-		if(a == 0) begin
-			target_b = target_b + 1;
-			if(target_b == 10) begin
-				target_b <= 0;
-				a <= 1;
-			end
-		end
-	end
+always @(negedge photo_array[8]) begin
+	score <= score + 32'd250;
+	target_a <= 9;
+end
+
+always @(negedge photo_array[9]) begin
+	target_b <= 8;
 end
 
 endmodule
