16f37c30e3e7ad5d750ed7c82907468f1395a272
unknown <hglr24@gmail.com>
2019-12-04 03:35:22 -0500

    Fixed everything

--- laser_driver.v
+++ laser_driver.v
@@ -8,16 +8,17 @@ module laser_driver(clock, in, out);
 	
 	initial begin
 		cooldown <= 32'd0;
+		outreg <= 1;
 	end
 	
 	always @(posedge clock) begin
 		if (in == 0 && cooldown == 32'd0) begin
-			outreg <= 0;
+			outreg <= 0; // on
 			cooldown <= cooldown + 32'd1;
 		end else if (cooldown >= 32'd200000000) begin // then 2 seconds laser OFF (cooldown)
 			cooldown <= 32'd0;
 		end else if (cooldown >= 32'd100000000) begin // 2 seconds laser ON
-			outreg <= 1;
+			outreg <= 1; // off
 			cooldown <= cooldown + 32'd1;	
 		end else if (cooldown > 0)
 			cooldown <= cooldown + 32'd1; // increment cooldown counter
---
+++ mif_files/test.s
@@ -0,0 +1,4 @@
+loop:
+addi $t0active $r0 3
+addi $t1active $r0 7
+j loop
